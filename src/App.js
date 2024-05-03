903e3d89bee3a52b7c09981acc291455eb12c7a2
Gayathri Thejus <gayathrithejus@gmail.com>
2024-05-04 00:58:07 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './index.css'
+import Navbar from "./components/Navbar";
+ 
+import {auth} from './firebase';
+import {useAuthState} from 'react-firebase-hooks/auth';
 
 function App() {
+  const [user]=useAuthState(auth);
+  // console.log(user)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="App">
+    <div className="max-w-[1020px] text-center mx-auto ">
+      <section className="flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative ">
+      <Navbar/>
+      </section>
+
     </div>
+    
+   </div>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Signin from './Signin'
+import {auth} from '../firebase'
+import {useAuthState} from 'react-firebase-hooks/auth'
+
+
+const Navbar = () => {
+const [user] = useAuthState(auth)
+console.log(user)
+  return (
+    <div className='bg-gray-800 h-20 flex justify-between items-center p-4'>
+      <h1 className='text-white text-3xl'>Chat App</h1>
+       <Signin />
+
+    </div>
+  );
+};
+
+export default Navbar;
